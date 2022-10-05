52d6420d148b214cb5d6fefc2037f0aa494b3081
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-10-05 19:27:18 +0300

    Python: Hello World, Print ve Aciklama Satiri Dosyalari Eklendi

---
+++ 007-Python-Kategorisi/004-print-Kullanimi-ve-Aciklama-Satiri-Eklemek.py
@@ -0,0 +1,27 @@
+# Print Kullanımı ve Açıklama Satırları Eklemek
+# Aciklama Satirlari Sadece 1 Satir Olabilir
+"""
+ofxjg osjdgf
+sdgpodfjg pojdfg
+sdojgfpodsjkgposdjkg
+dsfgokdspgokdfs
+"""
+
+print("Hello World") # Aciklama Bilgisi Eklendi
+
+# TODO: Todo ekledik ;)
+
+print(1, 2, 3, 4, 5) # birden fazla bilgi print ile birlikte virgul ile ayrilarak yazilabilir
+print("deneme", "ikinci bilgi", "ucuncu bilgi")
+print(20 * 20)
+print("*" * 30)
+print("-" * 30)
+# 1. Ornek:  100 * 100 ekrana yazilmayacaktir
+100 * 100
+print("sonuc yukaridaki gibidir")
+
+
+print("-" * 30)
+# 2. Ornek:  100 * 100 ekrana yazilacak
+print(100 * 100) # print ile ekrana sonuc yazdirildi
+print("sonuc yukaridaki gibidir")
