62ca7fce4f2cf5783bc604f87ed7d0b494144c97
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-10-06 17:59:57 +0300

    Python: Degisken Tanimlama Kurallari

---
+++ 007-Python-Kategorisi/007-Degisken-Tanimlama-ve-Degisken-Tanimlama-Kurallari.py
@@ -0,0 +1,34 @@
+# https://winnercrespo.com/naming-conventions/
+
+# Degisken Isimleri Nasil Olmalidir ?
+# g14 = "Python"  # bu degisken adi anlasilir mi ?
+# print(g14)
+x = 1
+# x, i, y, z gibi degisken isimleri kullanmayin
+
+# Degisken Isimleri Ingilizce mi Olmali?
+# kullanıcı_adı = "Gamze"
+first_name = "Gamze" # Lutfen degisken isimlerini Ingilizce Kullanalim
+
+# Degisken Isimleri Rakamla Baslayabilir mi?
+# 123_deneme = "Deneme" -> Rakamla Baslayamaz
+d_1234 = "Deneme" # bu kullanilabilir
+
+# Degisken Isimleri Buyuk Harfle Yazilabilir mi?
+password = "1234" # BUYUK HARFLE OLMALI MIYDI
+SERVER_IP = "127.0.0.1"
+
+# Degisken Isimleri Birden Fazla Kez Kullanilabilir mi?
+print(first_name)
+first_name = "Aylin"
+print(first_name)
+
+# Degisken Isimlerini Buyuk Harf Yazmakla Kucuk Harfle Yazmak Arasinda Fark Var mi?
+# Eger buyuk harfle yaziliyorsa genellikle bu bilgi degismeyecek bilgidier..
+
+# Degisken Isimleri Buyuk Harfle(USER_NAME) ve Kucuk Harfle(user_name) Yazilsa da Ayni Sonucu Alir miyiz?
+user_name = "lorem ipsum"
+user_Name = "lorem ipsum dddddddkhudihd iuhdiuhdiud"
+USER_NAME = "lorem 2"
+
+print(user_name)
