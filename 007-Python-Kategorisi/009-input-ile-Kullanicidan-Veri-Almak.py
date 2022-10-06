f83a494588edc96773237989f88e1d2e78f03dff
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-10-06 19:45:34 +0300

    Python: input kullanimi

---
+++ 007-Python-Kategorisi/009-input-ile-Kullanicidan-Veri-Almak.py
@@ -0,0 +1,11 @@
+# input kullanimi
+name = input("Adinizi Giriniz: ")
+
+# bu bilgi nerde ???
+print(name)
+print(name)
+print(name)
+
+# input ile gelen bilginin tipi nedir?
+age = input("Yasin Kac? ")  # input ile gelen tum bilgiler str olarak aktarilir.
+print( type(age), age )
