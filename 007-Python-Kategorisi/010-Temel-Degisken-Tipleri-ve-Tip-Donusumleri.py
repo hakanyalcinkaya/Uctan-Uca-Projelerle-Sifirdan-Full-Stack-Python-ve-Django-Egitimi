928b873be649731387aa6fd8c4444b955c8980a3
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-10-06 19:46:11 +0300

    Python: Temel Degisken Tipleri ve Tip Donusumleri

---
+++ 007-Python-Kategorisi/010-Temel-Degisken-Tipleri-ve-Tip-Donusumleri.py
@@ -0,0 +1,28 @@
+# Metinsel ifadeler -> str
+first_name = "Hakan"
+print("first_name", first_name, type(first_name))
+
+# Tam Sayilar -> int
+age = 33
+print("age", age, type(age) )
+
+
+# Virgullu Sayilar -> float
+tax = 1199 * 0.18
+print("tax", tax, type(tax))
+
+# True / False -> bool
+is_active = True
+print("is_active", is_active, type(is_active))
+
+# Metinden Tam Sayi Donusumu
+age = input("Yasin Kac:  ")
+age = int(age)
+print("age", age, type(age) )
+
+# Virgullu Sayidan Tam Sayiya Donusum
+tax = int(tax)
+print("tax", tax, type(tax))
+
+# Boolean Donusumu
+print(bool(tax))
