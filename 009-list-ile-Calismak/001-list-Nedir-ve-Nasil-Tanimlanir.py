0b44f4f0d43788edc0c398a7ab7397a79539e5ab
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-10-09 00:22:42 +0300

    Python: list Nedir ve Nasil Tanimlanir

---
+++ 009-list-ile-Calismak/001-list-Nedir-ve-Nasil-Tanimlanir.py
@@ -0,0 +1,31 @@
+# Bos Liste Nasil Olusturulur
+# users = []
+users = list()
+print(users)
+
+# Icinde Bilgi Olan Liste Nasil Olusturulur?
+items = [1, 2, 3, 43,]
+print(items)
+
+# list Hangi Turde Verileri Kabul Eder?
+item = "laptop"
+
+new_items = [
+    1,
+    2,
+    3,
+    bool(""),
+    bool(item),
+    4,
+    'item',
+    5,
+    6,
+    "django",
+    "pelin",
+    10.11,
+    item,
+    True,
+    False,
+]
+
+print(new_items)
