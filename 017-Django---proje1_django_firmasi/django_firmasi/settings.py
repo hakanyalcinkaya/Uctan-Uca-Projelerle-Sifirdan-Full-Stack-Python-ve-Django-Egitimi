879747ab94f19b26ff8976b43657cd99384fb29a
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-23 21:06:02 +0300

    Django: 1.Proje: Ana Sayfa HTML render

--- 017-Django---proje1_django_firmasi/django_firmasi/settings.py
+++ 017-Django---proje1_django_firmasi/django_firmasi/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    # My Apss:
+    'page',
 ]
 
 MIDDLEWARE = [
--- 017-Django---proje1_django_firmasi/page/views.py
+++ 017-Django---proje1_django_firmasi/page/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from random import randint
 
 
 def home(request):
     # print("request:::", request.META)
     # print("request:::", request.HEADERS)
-    return HttpResponse('Ana Sayfaya Hosgeldiniz..')
+    context = {"platform": f"Django Platformu Kullanildi ve randint ile donen veri:{randint(1, 100)} "}
+    return render(request, "page/home_page.html", context)
