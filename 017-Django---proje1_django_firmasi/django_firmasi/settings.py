0b3984ffe986064e19238630db9ce1c8268e0ada
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-24 20:55:10 +0300

    static yapisi olusturuldu

--- 017-Django---proje1_django_firmasi/django_firmasi/settings.py
+++ 017-Django---proje1_django_firmasi/django_firmasi/settings.py
@@ -120,6 +120,10 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [
+    BASE_DIR / 'static_files'
+]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
