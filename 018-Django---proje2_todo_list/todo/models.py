162fb1ce76cdb0f350d4ae872d7bd29ee8d9a840
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-25 17:13:23 +0300

    Django: Proje2: todo model ve admin registration eklendi

--- 018-Django---proje2_todo_list/todo/models.py
+++ 018-Django---proje2_todo_list/todo/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=200)
+    content = models.TextField(blank=True, null=True)
+    is_active = models.BooleanField(default=False)
