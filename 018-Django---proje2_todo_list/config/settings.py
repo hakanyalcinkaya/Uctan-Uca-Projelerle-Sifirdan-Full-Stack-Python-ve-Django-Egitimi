2df4542d63584cd1f9697c90a0d9e12d34581fbe
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-25 16:53:56 +0300

    Django: Proje2: todo app eklendi

--- 018-Django---proje2_todo_list/config/settings.py
+++ 018-Django---proje2_todo_list/config/settings.py
@@ -30,7 +30,7 @@ ALLOWED_HOSTS = []
 
 # Application definition
 
-INSTALLED_APPS = [
+BASE_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -38,6 +38,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+THIRD_PARTY_APPS = [
+
+]
+
+MY_APPS = [
+    'todo',
+]
+
+INSTALLED_APPS = BASE_APPS + THIRD_PARTY_APPS + MY_APPS
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
