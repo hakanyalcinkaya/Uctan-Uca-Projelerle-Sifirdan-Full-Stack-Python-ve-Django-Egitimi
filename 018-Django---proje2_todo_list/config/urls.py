0d34cd3794abdf073c98d3bd69301442b68d3f0d
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-26 19:01:17 +0300

    Django: Proje2: logout-Yapisi-Eklendi

--- 018-Django---proje2_todo_list/config/urls.py
+++ 018-Django---proje2_todo_list/config/urls.py
@@ -15,12 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from config.views import logout_view
 from todo.views import home_view, todo_detail_view, category_view
 
 urlpatterns = [
     path('', home_view),
     path('category/<slug:category_slug>/', category_view, name='category_view'),
     path('category/<slug:category_slug>/todo/<int:id>/', todo_detail_view, name='todo_detail_view'),
+    path('logout/', logout_view, name='logout_view'),
     # path('todo/<int:id>/', todo_detail_view, name='todo_detail_view'),
     path('admin/', admin.site.urls),
 ]
