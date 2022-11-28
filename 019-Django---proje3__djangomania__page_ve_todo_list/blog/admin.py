9305f02385844ef91482c31a2c971e37ec2604e0
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-28 18:12:03 +0300

    Django: Proje3: Blog-Yapisinin-Admin-Panele-Eklenmesi

--- 019-Django---proje3__djangomania__page_ve_todo_list/blog/admin.py
+++ 019-Django---proje3__djangomania__page_ve_todo_list/blog/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import BlogCategory, BlogTag, Post
 
-# Register your models here.
+
+@admin.register(BlogTag)
+class BlogTagAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(BlogCategory)
+class BlogCategoryAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'title',
+        'is_active',
+    ]
