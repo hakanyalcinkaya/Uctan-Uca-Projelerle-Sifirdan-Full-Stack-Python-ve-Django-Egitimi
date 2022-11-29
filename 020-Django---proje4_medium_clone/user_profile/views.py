0651eb9547d514549e037d845aee3b488e452875
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-29 19:10:25 +0300

    Django: Proje4: Login-Denetimlerinin-Eklenmesi

--- 020-Django---proje4_medium_clone/user_profile/views.py
+++ 020-Django---proje4_medium_clone/user_profile/views.py
@@ -4,18 +4,19 @@ from django.shortcuts import render, redirect
 
 
 def login_view(request):
-    # login olan kullanici direkt olaran ana sayfaya gitsin..
     if request.user.is_authenticated:
         messages.info(request, f'{request.user.username } Daha Once Login Olmussun ;)')
         return redirect('home_view')
 
     context = dict()
     if request.method == "POST":
-        # print(request.POST)
         username = request.POST.get('username')
         password = request.POST.get('password')
-        print(username, password)
-        # Bu bilgileri dogru aldik mi?
+        
+        if len(username) < 6 or len(password) < 6:
+            messages.warning(request, f'Lutfen Kullani Adi ve Sifreyi Dogru Giriniz.. 6 Karakterden Kucuk Olmamali..')
+            return redirect('user_profile:login_view')
+
         user = authenticate(request, username=username, password=password)
         if user is not None:
             login(request, user)
