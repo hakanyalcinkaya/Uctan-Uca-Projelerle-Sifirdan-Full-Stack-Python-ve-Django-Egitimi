296b22381eafdffb887092414a09888b0412adb5
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-29 22:18:01 +0300

    Ana-Sayfanin-Duzenlenmesi-ve-Form-Yapisina-Class-Eklenmesi

--- 020-Django---proje4_medium_clone/blog/forms.py
+++ 020-Django---proje4_medium_clone/blog/forms.py
@@ -4,6 +4,7 @@ from .models import Post
 
 class PostModelForm(forms.ModelForm):
     tag = forms.CharField()
+
     class Meta:
         model = Post
         fields = [
@@ -13,3 +14,7 @@ class PostModelForm(forms.ModelForm):
             'category',
             'tag',
         ]
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['title'].widget.attrs.update({'class': 'form-control'})
+        self.fields['category'].widget.attrs.update({'class':'form-control'})
