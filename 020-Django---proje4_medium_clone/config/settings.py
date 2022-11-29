70e9c35b44bc5f076a1987f10b6d3bda50dcffad
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-29 17:39:23 +0300

    Django: Proje4: Ana-Sayfanin-Settings-Icerisinde-Ayarlarinin-Yapilmasi-ve-Static-Files-Duzenlemeleri

--- 020-Django---proje4_medium_clone/config/settings.py
+++ 020-Django---proje4_medium_clone/config/settings.py
@@ -64,7 +64,9 @@ ROOT_URLCONF = 'config.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -126,6 +128,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static_files",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
