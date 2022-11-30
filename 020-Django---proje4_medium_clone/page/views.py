b74e15c584c31597f7348b6945e720c08b6533c3
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-30 19:25:13 +0300

    Django: Proje4: Post-Bilgilerinin-Shell-Ekranindan-Olusturulmasi-ve-En-Cok-Okunan-6-Post-un-Sorgusu

--- 020-Django---proje4_medium_clone/page/views.py
+++ 020-Django---proje4_medium_clone/page/views.py
@@ -4,11 +4,13 @@ from blog.models import BlogPost, Tag, Category
 
 def home_view(request):
     posts = BlogPost.objects.filter(is_active=True) # .order_by('-created_at')
+    top_posts = posts.order_by('-view_count')[:6]
     tags = Tag.objects.filter(is_active=True)
     categories = Category.objects.filter(is_active=True)
     context = dict(
         categories=categories,
         posts=posts,
         tags=tags,
+        top_posts=top_posts,
     )
     return render(request, 'page/home_page.html', context)
