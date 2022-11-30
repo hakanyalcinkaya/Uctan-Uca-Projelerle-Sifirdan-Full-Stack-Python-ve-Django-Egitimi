57129585efc7a46d76761728d921cfa6638eaf7e
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2022-11-30 16:35:34 +0300

    Django: Proje4: Kullanici-Profiline-get_absolute_url-ile-Ulasilabilmesi

--- 020-Django---proje4_medium_clone/read/views.py
+++ 020-Django---proje4_medium_clone/read/views.py
@@ -6,6 +6,7 @@ from user_profile.models import Profile
 def all_posts_view(request, user_slug):
     profile = get_object_or_404(Profile, slug=user_slug)
     context = dict(
+        profile=profile,
         posts=BlogPost.objects.filter(user=profile.user, is_active=True)
     )
     return render(request, 'read/all_posts.html', context)
--- 020-Django---proje4_medium_clone/user_profile/models.py
+++ 020-Django---proje4_medium_clone/user_profile/models.py
@@ -1,3 +1,4 @@
+from django.urls import reverse
 from django.db import models
 from django.contrib.auth.models import User
 from autoslug import AutoSlugField
@@ -7,3 +8,11 @@ class Profile(models.Model):
     avatar = models.ImageField(upload_to='avatar')
     instagram = models.CharField(max_length=200)
     slug = models.SlugField(max_length=200)
+
+    def get_absolute_url(self):
+        return reverse(
+            'read:all_posts_view',
+            kwargs={
+                "user_slug": self.slug,
+            }
+        )
