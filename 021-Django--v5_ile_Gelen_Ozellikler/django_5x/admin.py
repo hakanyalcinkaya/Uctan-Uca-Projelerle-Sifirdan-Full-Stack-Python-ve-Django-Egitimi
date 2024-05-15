ab1eac34e6f1a3886329b9e9d0d6c6be9b7374d1
Hakan Yalcinkaya <hakanyalcinkaya@gmail.com>
2024-05-14 21:17:33 -0300

    Django 5x: veri secim ozellikleri

--- 021-Django--v5_ile_Gelen_Ozellikler/django_5x/admin.py
+++ 021-Django--v5_ile_Gelen_Ozellikler/django_5x/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import User
+from .models import User, Winner
 
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
@@ -8,4 +8,15 @@ class UserAdmin(admin.ModelAdmin):
         'full_name',
         'age',
         'created_at',
+    ]
+
+
+@admin.register(Winner)
+class WinnerAdmin(admin.ModelAdmin):
+    list_display = [
+        'id',
+        'name',
+        'medal',
+        'sport',
+        'score',
     ]
--- 021-Django--v5_ile_Gelen_Ozellikler/django_5x/models.py
+++ 021-Django--v5_ile_Gelen_Ozellikler/django_5x/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.db.models.functions import Now
+from faker import Faker
 
 
 class User(models.Model):
@@ -10,3 +11,27 @@ class User(models.Model):
     age = models.SmallIntegerField(db_default=18)
     created_at=models.DateTimeField(db_default=Now())
 
+
+Medal = models.TextChoices("Medal", "GOLD SILVER BRONZE")
+
+SPORT_CHOICES = {  # Using a mapping instead of a list of 2-tuples.
+    "running": "Running",
+    "Martial Arts": {"judo": "Judo", "karate": "Karate"},
+    "Racket": {"badminton": "Badminton", "tennis": "Tennis"},
+    "unknown": "Unknown",
+}
+
+def get_scores():
+    return [(i, str(i)) for i in range(1, 11)]
+
+def get_jobs():
+    faker = Faker()
+    return [(faker.job(), faker.job().upper()) for i in range(10)]
+
+
+class Winner(models.Model):
+    name = models.CharField(max_length=20)
+    medal = models.CharField(max_length=20, choices=Medal)  # Using `.choices` not required.
+    sport = models.CharField(max_length=20, choices=SPORT_CHOICES)
+    score = models.IntegerField(choices=get_scores)
+    job = models.CharField(max_length=100, default="", choices=get_jobs)
